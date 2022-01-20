65a2734f9cd74b2d0d6c9f27f8bcc3a7a8a27c45
ProfEduardo <profeduardo67.cap@gmail.com>
2022-01-20 15:10:28 -0600

    Se cambia la lógica del sidebar a su módulo, y se genera el puntero para los li
    Se cambia todo el código generado para el menu laterar, a su sidebar component, para modular, y se pone estilo al puntero para los li solo del sidebar

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -3,7 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
+  /*Esto afectará solo a los li, que esten en este componente no en general a todos los li de la aplicación */
   styles: [
+    
+    `
+        li{
+          cursor: pointer;
+        }
+    
+    
+    `
   ]
 })
 export class SidebarComponent implements OnInit {

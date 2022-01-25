71e8fc59d8469679f9ba2c951dab21bf1342940c
ProfEduardo <profeduardo67.cap@gmail.com>
2022-01-24 18:51:05 -0600

    Se maneja el error
    Manejo de error, para que solo muestre el mensaje cuando no haya coincidencia en la busqueda, y la tabla se mostrará solo cuando haya coincidencia

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -12,15 +12,25 @@ export class PorPaisComponent{
    /**Definimos la propiedad para utilizar el NgModel, del html */
    termino: string = '';
 
+   /**Para el manejo de errores creamos otra propiedad */
+  hayError: boolean = false;
+
+
   constructor(private paisService: PaisService) { }//Aca inyectamos nuesto servicio, para poder consumirlo
 
   /**Implementamos el método de buscar */
   buscar(){
+    this.hayError = false;
     console.log(this.termino);
 
     //hay que recordar que para que un observable se dispare, hay que tener almenos un subscribe
-    this.paisService.buscarPais(this.termino).subscribe(resp => {
+    this.paisService.buscarPais(this.termino).subscribe(resp => {//En este caso del observable, esta seria la parte del next
       console.log(resp);//Por ahorita mandamos a pintar el resultado de la búsqueda
+    }, (err) => {
+      this.hayError = true;
+
+      // console.log('Error');
+      // console.info(err);
     });
   }
 

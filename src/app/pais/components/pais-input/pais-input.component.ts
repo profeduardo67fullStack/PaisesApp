09520e663b7385a26d02c3222f789d1d5b00faf2
ProfEduardo <profeduardo67.cap@gmail.com>
2022-02-07 14:23:42 -0600

    Se corrigen errores
    Se corrigen errores para que volviera a funcionar la busqueda una vez que separamos el Imput a un bloque

--- src/app/pais/components/pais-input/pais-input.component.ts
+++ src/app/pais/components/pais-input/pais-input.component.ts
@@ -8,12 +8,9 @@ import { Component,EventEmitter, Output } from '@angular/core';
 })
 export class PaisInputComponent {
 
-  @Output() onEnter: EventEmitter<string> = new EventEmitter  //Con esto vamos a emitir el this.termino,para que pueda ser enviado
+  @Output() onEnter: EventEmitter<string> = new EventEmitter();  //Con esto vamos a emitir el this.termino,para que pueda ser enviado
   
-  
-  
-
-  termino: string ="";
+      termino: string ="";
 
   buscar(){
     this.onEnter.emit(this.termino);//Cuando se ocupe el método buscar con el enter, se va a emitir el this.termino
